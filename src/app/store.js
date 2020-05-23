4de2dda2c090f19256467e28b8e39fb6936505db
Daniel Corcoran <danielc.92@outlook.com>
2020-05-24 09:45:09 +1000

    Added logger middleware

--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,14 @@
-import { configureStore } from '@reduxjs/toolkit';
-import counterReducer from '../features/counter/counterSlice';
+import { configureStore, getDefaultMiddleware } from "@reduxjs/toolkit"
+import counterReducer from "../features/counter/counterSlice"
+import { createLogger } from "redux-logger"
+
+const logger = createLogger({
+  // ...options
+})
 
 export default configureStore({
   reducer: {
     counter: counterReducer,
   },
-});
+  middleware: [logger, ...getDefaultMiddleware()],
+})
