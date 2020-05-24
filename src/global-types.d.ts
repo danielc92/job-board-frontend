8a06633d3da30968904a611ff3817d0478307f10
Daniel Corcoran <danielc.92@outlook.com>
2020-05-24 18:59:47 +1000

    Added some global types

--- src/global-types.d.ts
+++ src/global-types.d.ts
@@ -10,3 +10,24 @@ interface IPayloadLogin {
       title: string
       content: string
     }
+
+
+    interface IPayloadNewsList extends ParsedUrlQueryInput {
+      page: string
+    }
+
+    
+    interface IPaginatedData<T> {
+      results: {
+        docs: T[]
+        totalDocs: number
+        limit: number
+        totalPages: number
+        page: number
+        pagingCounter: number
+        hasPrevPage: boolean
+        hasNextPage: boolean
+        prevPage: boolean | null
+        nextPage: boolean | null
+      }
+    }
