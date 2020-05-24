a358dba7f2498d719702117e9eec41be82608fcb
Daniel Corcoran <danielc.92@outlook.com>
2020-05-24 13:23:25 +1000

    Added settings

--- src/settings/index.ts
+++ src/settings/index.ts
@@ -1,12 +1,8 @@
-export const settings = {
-  constants: {
-    TOKEN_NAME: "AUTH_TOKEN",
-    SITE_NAME: "Applymer",
-    SERVER_500_ERROR_MESAGE:
-      "We apologise for the inconvenience. The server was unable to process your request.",
-    ALLOWED_CHARS_HUMAN_NAME: "abcdefghijklmnopqrstuvwxyz ",
-    ALLOWED_CHARS_JOB: "abcdefghijklmnopqrstuvwxyz1234567890- .,",
-    ESSION_EXPIRED_MESSAGE:
-      "We apologise, your session has expired, this is a security measure to keep your information safe. Please login again.",
-  },
-}
+export const SITE_NAME = 'Applymer'
+export const TOKEN_NAME = 'JOB_TOKEN'
+export const SERVER_500_ERROR_MESAGE =
+  'We apologise for the inconvenience. The server was unable to process your request.'
+export const SESSION_EXPIRED_MESSAGE =
+  'We apologise, your session has expired, this is a security measure to keep your information safe. Please login again.'
+export const ALLOWED_CHARS_JOB = 'abcdefghijklmnopqrstuvwxyz1234567890- .,'
+export const ALLOWED_CHARS_HUMAN_NAME = 'abcdefghijklmnopqrstuvwxyz '
