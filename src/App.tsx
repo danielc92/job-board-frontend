3315093927bf7bd68207ca69d7c851d0895a0d66
Daniel Corcoran <danielc.92@outlook.com>
2020-05-24 18:59:39 +1000

    Added two new routes

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,9 @@ import React, { Fragment } from "react"
 import { Route } from "react-router-dom"
 import HomePage from "pages/home"
 import SigninPage from "pages/signin"
+import RegisterPage from "pages/register"
+import NewsListPage from "pages/news-list"
+
 interface IProps {}
 
 const App: React.FC<IProps> = () => {
@@ -9,6 +12,8 @@ const App: React.FC<IProps> = () => {
     <Fragment>
       <Route exact path="/" component={HomePage} />
       <Route exact path="/sign-in" component={SigninPage} />
+      <Route exact path="/register" component={RegisterPage} />
+      <Route exact path="/news-list" component={NewsListPage} />
     </Fragment>
   )
 }
