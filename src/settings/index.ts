2b6b24b4437ad34239f831816059df7ec881ea8a
Daniel Corcoran <danielc.92@outlook.com>
2020-05-24 11:44:38 +1000

    Added settings

---
+++ src/settings/index.ts
@@ -0,0 +1,12 @@
+export const settings = {
+  constants: {
+    TOKEN_NAME: "AUTH_TOKEN",
+    SITE_NAME: "Applymer",
+    SERVER_500_ERROR_MESAGE:
+      "We apologise for the inconvenience. The server was unable to process your request.",
+    ALLOWED_CHARS_HUMAN_NAME: "abcdefghijklmnopqrstuvwxyz ",
+    ALLOWED_CHARS_JOB: "abcdefghijklmnopqrstuvwxyz1234567890- .,",
+    ESSION_EXPIRED_MESSAGE:
+      "We apologise, your session has expired, this is a security measure to keep your information safe. Please login again.",
+  },
+}
