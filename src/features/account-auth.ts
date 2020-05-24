29e96f6572c9d7b991e22491b242ea84d58d19a3
Daniel Corcoran <danielc.92@outlook.com>
2020-05-24 13:23:07 +1000

    Fleshed out auth slice

--- src/features/account-auth.ts
+++ src/features/account-auth.ts
@@ -1,27 +1,39 @@
-import { createSlice, createAsyncThunk } from "@reduxjs/toolkit"
+import { createSlice, createAsyncThunk, PayloadAction } from "@reduxjs/toolkit"
 import { RootState } from "../app/store"
 import { api } from "api"
+import { helpers } from "helpers"
+import jwt_decode from "jwt-decode"
+import { TOKEN_NAME } from "settings"
 
 interface IStateAuth {
   isAuthenticated: boolean
   user: any
-  error: boolean
-  errorMessage: string
+  error: null | string
   isFetching: boolean
 }
 const initialState: IStateAuth = {
   isAuthenticated: false,
   user: null,
-  error: false,
-  errorMessage: "",
+  error: null,
   isFetching: false,
+}
+
+interface IApiLogin {
+  token: {
+    "x-auth-token": string
+  }
 }
 
 export const loginUser = createAsyncThunk(
   "account-auth-login",
-  async (args: IPayloadLogin) => {
-    const response = await api.post("auth/login", args)
-    return response
+  async (args: IPayloadLogin, thunkAPI) => {
+    try {
+      const response = await api.post<IApiLogin>("auth/login", args)
+      return response.data
+    } catch (error) {
+      const value = helpers.handleAxiosError(error)
+      return thunkAPI.rejectWithValue(value)
+    }
   }
 )
 export const accountAuthSlice = createSlice({
@@ -29,9 +41,27 @@ export const accountAuthSlice = createSlice({
   initialState,
   reducers: {},
   extraReducers: (buildler) => {
-    buildler.addCase(loginUser.fulfilled, (state, action) => {
-      
-    }), 
+    buildler
+      .addCase(loginUser.pending, (state, action) => {
+        state.error = null
+        state.user = null
+        state.isFetching = true
+        state.isAuthenticated = false
+      })
+      .addCase(loginUser.fulfilled, (state, action) => {
+        const { payload } = action
+        localStorage.setItem(TOKEN_NAME, payload.token["x-auth-token"])
+        state.error = null
+        state.user = jwt_decode(payload.token["x-auth-token"])
+        state.isFetching = false
+        state.isAuthenticated = true
+      })
+      .addCase(loginUser.rejected, (state, action) => {
+        state.error = action.payload as string
+        state.user = null
+        state.isFetching = false
+        state.isAuthenticated = false
+      })
   },
 })
 
