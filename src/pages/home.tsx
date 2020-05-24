aa80c66ec085fdcff72d3adccfe70206e1a89405
Daniel Corcoran <danielc.92@outlook.com>
2020-05-24 14:31:26 +1000

    Added sections to page

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,31 +1,49 @@
 import React, { Fragment } from "react"
 import { useSelector, useDispatch } from "react-redux"
-import { selectUser, loginUser } from "features/account-auth"
+import { selectUser } from "features/account-auth"
 import Footer from "sections/global/Footer"
+import Navbar from "sections/global/Navbar"
+import Features from "sections/home/Features"
+import Banner from "sections/home/Banner"
+import { SITE_NAME } from "settings"
+import Hero from "sections/home/Hero"
+import { featureData } from "./data"
 
 interface IProps {}
 
+
 const HomePage: React.FC<IProps> = () => {
-  const user = useSelector(selectUser)
-  const dispatch = useDispatch()
   return (
     <Fragment>
-      <button
-        onClick={() =>
-          dispatch(
-            loginUser({ email: "test2@test.com", password: "123456789d" })
-          )
-        }
-      >
-        Login
-      </button>
-      <button></button>
-      <p>
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Tenetur vel
-        debitis deserunt at accusantium molestias aspernatur dolorum dolore,
-        nobis consequuntur! Pariatur voluptatibus odio sapiente fuga id mollitia
-        est consequatur nam?
-      </p>
+      <Navbar />
+
+      <Banner
+        imageSrc="/images/undraw_interview_rmcf.svg"
+        ctaHeader={`Welcome to ${SITE_NAME}`}
+        ctaSubHeader="A community driven job board, helping people find the jobs they need."
+        buttonText="Explore jobs"
+      />
+      <Features data={featureData} />
+      <Hero
+        ctaHeader="Process"
+        ctaSubHeader="An easy to use, intuitive process for setting up job postings and applying for jobs."
+        image="/images/process_e90d.svg"
+        left
+      />
+
+      <Hero
+        ctaHeader="Personalization"
+        ctaSubHeader="Customize your profile, so that employers can find you easily, and you can get the job you want."
+        image="/images/success_factors_fay0.svg"
+        left={false}
+      />
+
+      <Hero
+        ctaHeader="Feedback"
+        ctaSubHeader="Our system is driven by feedback, ensuring the latest features are what matters to you."
+        image="/images/feedback.svg"
+        left
+      />
       <Footer />
     </Fragment>
   )
