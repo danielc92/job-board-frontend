f4af9f33bfb4f2a33e641962c57df15ed6c3230f
Daniel Corcoran <danielc.92@outlook.com>
2020-05-24 13:22:34 +1000

    Added reset state with LOGOUT_SUCCESS

--- src/app/rootReducer.ts
+++ src/app/rootReducer.ts
@@ -1,7 +1,16 @@
 import todos from "features/example"
 import accountAuth from "features/account-auth"
+import { combineReducers, Action } from "@reduxjs/toolkit"
+import { TOKEN_NAME } from "settings"
 
-export const rootReducer = {
+const appReducer = combineReducers({
   todos,
   accountAuth,
+})
+export const rootReducer = (state: any, action: Action) => {
+  if (action.type === "LOGOUT_SUCCESS") {
+    localStorage.removeItem(TOKEN_NAME)
+    state = undefined
+  }
+  return appReducer(state, action)
 }
