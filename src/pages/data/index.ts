123c30fe819cced94fdcd6fae9f61026e35647c3
Daniel Corcoran <danielc.92@outlook.com>
2020-05-24 14:31:33 +1000

    Added features data

---
+++ src/pages/data/index.ts
@@ -0,0 +1,40 @@
+export const featureData: Array<IFeatures> = [
+  {
+    key: "5",
+    icon: "paper plane outline",
+    title: "Diverse Range",
+    content: "Job creation through various skills, benefits and locations.",
+  },
+  {
+    key: "6",
+    icon: "gem outline",
+    title: "Quality Services",
+    content:
+      "We love to improve, making sure you receive high quality services.",
+  },
+
+  {
+    key: "1",
+    icon: "star outline",
+    title: "Feedback system",
+    content: "We hear your feedback and adapt to changes.",
+  },
+  {
+    key: "2",
+    icon: "tasks",
+    title: "Track and monitor",
+    content: "Real-time tracking of applications and postings.",
+  },
+  {
+    key: "3",
+    icon: "chart bar outline",
+    title: "Analytics",
+    content: "Metric driven analytics.",
+  },
+  {
+    key: "4",
+    icon: "address card outline",
+    title: "Profiles",
+    content: "Fill out a few details and improve your job search.",
+  },
+]
