fa1f02a045fcfa45c61d08a8ca649e5bf0418c8f
Daniel Corcoran <danielc.92@outlook.com>
2020-05-24 11:46:43 +1000

    Added Route

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
-import React from "react"
-
+import React, { Fragment } from "react"
+import { Route } from "react-router-dom"
+import HomePage from "pages/home"
 interface IProps {}
 
 const App: React.FC<IProps> = () => {
-  return <p>Sup dudes</p>
+  return (
+    <Fragment>
+      <Route exact path="/" component={HomePage}></Route>
+    </Fragment>
+  )
 }
 
 export default App
-
