81663cf126bdd991bd146369adb73d82102f5f30
Daniel Corcoran <danielc.92@outlook.com>
2020-05-24 14:31:03 +1000

    Add type

--- src/global-types.d.ts
+++ src/global-types.d.ts
@@ -2,3 +2,11 @@ interface IPayloadLogin {
     email: string
     password: string
   }
+
+  interface IFeatures {
+    
+      key: string
+      icon: any
+      title: string
+      content: string
+    }
