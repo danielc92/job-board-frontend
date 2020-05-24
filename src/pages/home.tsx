53f51b6d685a62dd60b887ee95fcc86e94169bed
Daniel Corcoran <danielc.92@outlook.com>
2020-05-24 11:47:39 +1000

    Added Homepage boilerplate

---
+++ src/pages/home.tsx
@@ -0,0 +1,35 @@
+import React, { Fragment } from "react"
+import { useSelector, useDispatch } from "react-redux"
+import { selectUser, loginUser } from "features/account-auth"
+import Footer from "sections/global/Footer"
+
+interface IProps {}
+
+const HomePage: React.FC<IProps> = () => {
+  const user = useSelector(selectUser)
+  const dispatch = useDispatch()
+  console.log(user)
+  return (
+    <Fragment>
+      <button
+        onClick={() =>
+          dispatch(
+            loginUser({ email: "test2@test.com", password: "123456789" })
+          )
+        }
+      >
+        Login
+      </button>
+      <button></button>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Tenetur vel
+        debitis deserunt at accusantium molestias aspernatur dolorum dolore,
+        nobis consequuntur! Pariatur voluptatibus odio sapiente fuga id mollitia
+        est consequatur nam?
+      </p>
+      <Footer />
+    </Fragment>
+  )
+}
+
+export default HomePage
