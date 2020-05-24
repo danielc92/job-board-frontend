13d08caa1ced23fd8ee075c552fe313b3a198bb8
Daniel Corcoran <danielc.92@outlook.com>
2020-05-24 21:11:17 +1000

    Added benefits slice

---
+++ src/features/benefits.ts
@@ -0,0 +1,63 @@
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit"
+import { RootState } from "../app/store"
+import { api } from "api"
+import { handleAxiosError } from "utils/api"
+
+interface IApiBenefit {
+  name: string
+  _id: string
+}
+
+interface IStateBenefits {
+  error: null | string
+  isFetching: boolean
+  benefits: IApiBenefit[] | null
+}
+const initialState: IStateBenefits = {
+  error: null,
+  isFetching: false,
+  benefits: null,
+}
+
+export const getBenefits = createAsyncThunk(
+  "benefit-list",
+  async (args: void, thunkAPI) => {
+    try {
+      const response = await api.get(`benefit/list`)
+      return response.data
+    } catch (error) {
+      const value = handleAxiosError(error)
+      return thunkAPI.rejectWithValue(value)
+    }
+  }
+)
+
+export const benefitSlice = createSlice({
+  name: "benefit-list",
+  initialState,
+  reducers: {
+    reset: (state) => initialState,
+  },
+  extraReducers: (buildler) => {
+    buildler
+      .addCase(getBenefits.pending, (state, action) => {
+        state.error = null
+        state.isFetching = true
+        state.benefits = null
+      })
+      .addCase(getBenefits.fulfilled, (state, action) => {
+        state.error = null
+        state.isFetching = false
+        state.benefits = action.payload
+      })
+      .addCase(getBenefits.rejected, (state, action) => {
+        state.error = action.payload as string
+        state.isFetching = false
+        state.benefits = null
+      })
+  },
+})
+
+export const selectBenefits = (state: RootState) => state.benefit
+
+export default benefitSlice.reducer
