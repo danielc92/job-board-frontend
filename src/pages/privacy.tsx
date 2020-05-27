9ac98b9bf3f78ff5eb46127493b7af9240efe04f
Daniel Corcoran <danielc.92@outlook.com>
2020-05-27 21:12:46 +1000

    Added privacy Page

---
+++ src/pages/privacy.tsx
@@ -0,0 +1,73 @@
+import React, { useEffect, Fragment } from "react"
+import { useSelector, useDispatch } from "react-redux"
+import { Container, Placeholder, Segment } from "semantic-ui-react"
+import ErrorMessage from "sections/global/ErrorMessage"
+import {
+  selectDocumentation,
+  getDocumentationList,
+} from "features/documentation"
+import VerticallyPaddedContainer from "sections/global/VerticallyPaddedContainer"
+import { documentationRenderer } from "utils/render"
+import Footer from "sections/global/Footer"
+import BannerGroup from "sections/global/BannerGroup"
+import Navbar from "sections/global/Navbar"
+
+interface IProps {}
+
+const PrivacyPage: React.FC<IProps> = () => {
+  const dispatch = useDispatch()
+  const documentation = useSelector(selectDocumentation)
+
+  useEffect(() => {
+    dispatch(getDocumentationList())
+  }, [dispatch])
+
+  return (
+    <Fragment>
+      <Navbar />
+      <Container text>
+        <VerticallyPaddedContainer size="4">
+          {documentation.error ? (
+            <ErrorMessage
+              header="Error"
+              content="Something went wrong please try again later."
+            />
+          ) : documentation.isFetching ? (
+            <Fragment>
+              <Placeholder fluid>
+                <Placeholder.Header>
+                  <Placeholder.Line />
+                  <Placeholder.Line />
+                </Placeholder.Header>
+              </Placeholder>
+              <Segment stacked padded>
+                <Placeholder fluid>
+                  <Placeholder.Paragraph>
+                    <Placeholder.Line /> <Placeholder.Line />
+                    <Placeholder.Line /> <Placeholder.Line />
+                    <Placeholder.Line /> <Placeholder.Line />
+                    <Placeholder.Line />
+                    <Placeholder.Line /> <Placeholder.Line />
+                    <Placeholder.Line /> <Placeholder.Line />
+                    <Placeholder.Line /> <Placeholder.Line />
+                    <Placeholder.Line /> <Placeholder.Line />
+                    <Placeholder.Line /> <Placeholder.Line />
+                  </Placeholder.Paragraph>
+                </Placeholder>
+              </Segment>
+            </Fragment>
+          ) : (
+            documentationRenderer(
+              documentation.documentation?.results,
+              "privacy-policy"
+            )
+          )}
+        </VerticallyPaddedContainer>
+      </Container>
+      <BannerGroup showFeedback />
+      <Footer />
+    </Fragment>
+  )
+}
+
+export default PrivacyPage
