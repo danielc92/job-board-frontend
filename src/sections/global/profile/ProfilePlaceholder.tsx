3bce30a3224cf0e9a2fb6c9978beea5cccf23d93
Daniel Corcoran <danielc.92@outlook.com>
2020-05-29 13:50:35 +1000

    Added placeholder

---
+++ src/sections/global/profile/ProfilePlaceholder.tsx
@@ -0,0 +1,76 @@
+import React from "react"
+import { Placeholder, Segment } from "semantic-ui-react"
+const ProfilePlaceholder = () => {
+  return (
+    <React.Fragment>
+      <Placeholder fluid>
+        <Placeholder.Header>
+          <Placeholder.Line />
+          <Placeholder.Line />
+        </Placeholder.Header>
+      </Placeholder>
+
+      <Segment stacked padded>
+        <Placeholder fluid>
+          <Placeholder.Paragraph>
+            <Placeholder.Line />
+            <Placeholder.Line />
+            <Placeholder.Line />
+            <Placeholder.Line />
+            <Placeholder.Line />
+            <Placeholder.Line />
+
+            <Placeholder.Line />
+            <Placeholder.Line />
+          </Placeholder.Paragraph>
+        </Placeholder>
+      </Segment>
+      <Placeholder fluid>
+        <Placeholder.Header>
+          <Placeholder.Line />
+          <Placeholder.Line />
+        </Placeholder.Header>
+      </Placeholder>
+
+      <Segment stacked padded>
+        <Placeholder fluid>
+          <Placeholder.Paragraph>
+            <Placeholder.Line />
+            <Placeholder.Line />
+            <Placeholder.Line />
+            <Placeholder.Line />
+            <Placeholder.Line />
+            <Placeholder.Line />
+
+            <Placeholder.Line />
+            <Placeholder.Line />
+          </Placeholder.Paragraph>
+        </Placeholder>
+      </Segment>
+      <Placeholder fluid>
+        <Placeholder.Header>
+          <Placeholder.Line />
+          <Placeholder.Line />
+        </Placeholder.Header>
+      </Placeholder>
+
+      <Segment stacked padded>
+        <Placeholder fluid>
+          <Placeholder.Paragraph>
+            <Placeholder.Line />
+            <Placeholder.Line />
+            <Placeholder.Line />
+            <Placeholder.Line />
+            <Placeholder.Line />
+            <Placeholder.Line />
+
+            <Placeholder.Line />
+            <Placeholder.Line />
+          </Placeholder.Paragraph>
+        </Placeholder>
+      </Segment>
+    </React.Fragment>
+  )
+}
+
+export default ProfilePlaceholder
