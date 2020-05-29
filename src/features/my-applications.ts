b6d7e60656e85815c166fcffa7103286864847a8
Daniel Corcoran <danielc.92@outlook.com>
2020-05-29 19:49:38 +1000

    Added my applications slice

---
+++ src/features/my-applications.ts
@@ -0,0 +1,100 @@
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit"
+import { RootState } from "../app/store"
+import { api } from "api"
+import { handleAxiosError, getConfig } from "utils/api"
+
+interface IMyApplcation {
+  status: string
+  user_message: string
+  rating: number
+  _id: string
+  applicant_id: string
+  job_id: {
+    location: {
+      type: string
+      coordinates: number[]
+    }
+    benefits: string[]
+    skills: string[]
+    requirements: string[]
+    open: true
+    _id: string
+    creator_id: string
+    category: string
+    company_name: string
+    company_summary: string
+    contact_summary: string
+    employment_type: string
+    job_summary: string
+    location_string: string
+    salary_range_high: number
+    salary_range_low: number
+    slug: string
+    title: string
+    createdAt: string
+    updatedAt: string
+  }
+  createdAt: string
+  updatedAt: string
+}
+
+type IApiMyApplications = IPaginatedData<IMyApplcation>
+
+interface IStateMyApplications {
+  error: string | null
+  isFetching: boolean
+  myApplications: IApiMyApplications | null
+}
+
+const initialState: IStateMyApplications = {
+  error: null,
+  isFetching: false,
+  myApplications: null,
+}
+
+export const getMyApplcations = createAsyncThunk(
+  "my-applications",
+  async (args: { page?: string }, thunkAPI) => {
+    const config = getConfig()
+    try {
+      const response = await api.get(
+        `application/list${args.page ? `?page=${args.page}` : ""}`,
+        config
+      )
+      return response.data
+    } catch (error) {
+      const value = handleAxiosError(error)
+      return thunkAPI.rejectWithValue(value)
+    }
+  }
+)
+
+export const myApplicationsSlice = createSlice({
+  name: "my-applications",
+  initialState,
+  reducers: {
+    reset: (state) => initialState,
+  },
+  extraReducers: (buildler) => {
+    buildler
+      .addCase(getMyApplcations.pending, (state, action) => {
+        state.error = null
+        state.isFetching = true
+        state.myApplications = null
+      })
+      .addCase(getMyApplcations.fulfilled, (state, action) => {
+        state.error = null
+        state.isFetching = false
+        state.myApplications = action.payload
+      })
+      .addCase(getMyApplcations.rejected, (state, action) => {
+        state.error = action.payload as string
+        state.isFetching = false
+        state.myApplications = null
+      })
+  },
+})
+
+export const selectMyApplications = (state: RootState) => state.myApplications
+
+export default myApplicationsSlice.reducer
