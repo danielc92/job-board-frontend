901c722e9ca634dcc93b27a3783b33682a754ec0
Daniel Corcoran <danielc.92@outlook.com>
2020-06-04 20:35:08 +1000

    Refactored renderer

--- src/utils/render/index.tsx
+++ src/utils/render/index.tsx
@@ -7,42 +7,67 @@ import {
 } from "semantic-ui-react"
 import React, { Fragment } from "react"
 
+const NODES = {
+  HEADING_1: "heading-1",
+  HEADING_2: "heading-2",
+  HEADING_3: "heading-3",
+  HEADING_4: "heading-4",
+  HEADING_5: "heading-5",
+  HEADING_6: "heading-6",
+  PARAGRAPH: "paragraph",
+  UNORDERED_LIST: "unordered-list",
+  ORDERED_LIST: "ordered-list",
+  IMAGE: "image",
+}
+
+const spaceBottom = { marginBottom: "24px" }
+
 export const contentRenderer = (
   data: { node: string; value: string | Array<string> }[]
 ) => {
   if (!data) return null
 
-  const items = data.map((c) => {
-    if (c.node === "heading-3") {
-      return (
-        <Header style={{ marginBottom: "24px" }} as="h3" content={c.value} />
-      )
+  const items = data.map((item) => {
+    const { node, value } = item
+    switch (node) {
+      case NODES.HEADING_1:
+        return <Header style={spaceBottom} as="h1" content={value} />
+      case NODES.HEADING_2:
+        return <Header style={spaceBottom} as="h2" content={value} />
+      case NODES.HEADING_3:
+        return <Header style={spaceBottom} as="h3" content={value} />
+      case NODES.HEADING_4:
+        return <Header style={spaceBottom} as="h4" content={value} />
+      case NODES.HEADING_5:
+        return <Header style={spaceBottom} as="h5" content={value} />
+      case NODES.HEADING_6:
+        return <Header style={spaceBottom} as="h6" content={value} />
+      case NODES.PARAGRAPH:
+        return <p style={spaceBottom}>{value}</p>
+      case NODES.IMAGE:
+        return <Image style={spaceBottom} src={value} size="large" />
+      case NODES.UNORDERED_LIST:
+        let uListItems = (value as any).map((li: string) => <li>{li}</li>)
+        return <ul style={spaceBottom}>{uListItems}</ul>
+      case NODES.ORDERED_LIST:
+        let oListItems = (value as any).map((li: string) => <li>{li}</li>)
+        return <ol style={spaceBottom}>{oListItems}</ol>
+      default:
+        return <p>UNKNOWN NODE</p>
     }
-    if (c.node === "heading-1") {
-      return (
-        <Header style={{ marginBottom: "24px" }} as="h1" content={c.value} />
-      )
-    }
-    if (c.node === "paragraph") {
-      return <p style={{ marginBottom: "18px" }}>{c.value}</p>
-    }
-    if (c.node === "unordered-list") {
-      const listItems = (c as any).value.map((li: string) => <li>{li}</li>)
-      return <ul style={{ marginBottom: "24px" }}>{listItems}</ul>
-    }
-    if (c.node === "ordered-list") {
-      const listItems = (c as any).value.map((li: string) => <li>{li}</li>)
-      return <ol style={{ marginBottom: "24px" }}>{listItems}</ol>
-    }
-    if (c.node === "image") {
-      return (
-        <Image style={{ marginBottom: "24px" }} src={c.value} size="large" />
-      )
-    }
-    return <p>Unknown node.</p>
   })
+
   return items
 }
+
+//     if (c.node === "unordered-list") {
+//       const listItems = (c as any).value.map((li: string) => <li>{li}</li>)
+//       return <ul style={{ marginBottom: "24px" }}>{listItems}</ul>
+//     }
+//     if (c.node === "ordered-list") {
+//       const listItems = (c as any).value.map((li: string) => <li>{li}</li>)
+//       return <ol style={{ marginBottom: "24px" }}>{listItems}</ol>
+//     }
 
 export const documentationRenderer = (data: any, key: string) => {
   if (!data) return null
